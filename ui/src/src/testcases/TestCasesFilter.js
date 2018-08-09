15c719418e1b11690cc2d0f839a8ec7b35063899
azee <inge4hep>
2018-08-09 15:33:15 +0300

    Addable filters by attribute

--- ui/src/src/testcases/TestCasesFilter.js
+++ ui/src/src/testcases/TestCasesFilter.js
@@ -11,13 +11,19 @@ class TestCasesFilter extends Component {
         this.state = {
              filter: {
                  groups: [],
-                 filter: []
+                 filters: [{
+                    title: "Select an attribute",
+                    values: []
+                 }]
              },
              groupsToDisplay: [],
              projectAttributes: []
          };
 
         this.changeGrouping = this.changeGrouping.bind(this);
+        this.getValuesByAttributeId = this.getValuesByAttributeId.bind(this);
+        this.changeFilterAttributeId = this.changeFilterAttributeId.bind(this);
+        this.changeFilterAttributeValues = this.changeFilterAttributeValues.bind(this);
 
     }
 
@@ -28,26 +34,80 @@ class TestCasesFilter extends Component {
       this.setState(this.state);
     }
 
+    changeFilterAttributeId(index, value){
+        var oldId = this.state.filter.filters[index].id;
+        this.state.filter.filters[index].id = value.value;
+        this.state.filter.filters[index].name = value.label;
+        if (oldId !== value.value){
+            this.state.filter.filters[index].values = [];
+        }
+        if (!oldId && this.state.filter.filters.length < 4){
+            this.state.filter.filters.push({
+                id: null,
+                title: "Select an attribute",
+                values: []
+            })
+        }
+
+        this.setState(this.state);
+    }
+
+    changeFilterAttributeValues(index, values){
+        this.state.filter.filters[index].values = values.map(function(value){return value.value});
+        this.setState(this.state);
+    }
+
     changeGrouping(values){
         this.state.filter.groups = values.map(function(value){return value.value});
         this.state.groupsToDisplay = values;
-
-        console.log(this.state.groupsToDisplay);
-
         this.setState(this.state);
+    }
+
+    getValuesByAttributeId(id){
+        if(!id) return [];
+        return (this.state.projectAttributes.find(function(attribute){return attribute.id === id}) || {values: []}).values;
     }
 
 
     render() {
         return (
             <div>
-                <div>Grouping</div>
                 <div>
-                    <Select value={this.state.groupsToDisplay}
-                            isMulti
-                            onChange={this.changeGrouping}
-                            options={this.state.projectAttributes.map(function(val){return {value: val.id, label: val.name}})}
-                           />
+                    <div>Grouping</div>
+                    <div>
+                        <Select value={this.state.groupsToDisplay}
+                                isMulti
+                                onChange={this.changeGrouping}
+                                options={this.state.projectAttributes.map(function(val){return {value: val.id, label: val.name}})}
+                               />
+                    </div>
+                </div>
+                <div>
+                    <div>Filter</div>
+                    <div>
+
+                        {
+                            this.state.filter.filters.map(function(filter, i){
+                                return(
+                                    <div>
+                                        <Select value={{value: filter.id, label: filter.name}}
+                                                onChange={(e) => this.changeFilterAttributeId(i, e)}
+                                                options={this.state.projectAttributes.map(function(val){return {value: val.id, label: val.name}})}
+                                               />
+                                        <Select value={filter.values.map(function(value){return {value: value, label: value}})}
+                                                isMulti
+                                                onChange={(e) => this.changeFilterAttributeValues(i, e)}
+                                                options={this.getValuesByAttributeId(filter.id).map(function(val){return {value: val, label: val}})}
+                                               />
+                                    </div>
+                                )
+                            }.bind(this))
+
+                        }
+
+
+
+                    </div>
                 </div>
             </div>
         );
