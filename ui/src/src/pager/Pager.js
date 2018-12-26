b7425f49481a7d057074d270a708afbfd972d560
azee <inge4hep>
2018-12-26 23:33:34 +1100

    Start-end pagination correction

--- ui/src/src/pager/Pager.js
+++ ui/src/src/pager/Pager.js
@@ -22,23 +22,24 @@ class Pager extends Component {
     }
 
     componentWillReceiveProps(props) {
-      this.state.currentPage = props.currentPage;
-      this.state.pageObjects = this.getPageObjects();
-      this.setState(this.state);
+        this.state.totalItems = props.totalItems;
+        this.state.currentPage = props.currentPage;
+        this.state.pageObjects = this.getPageObjects();
+        this.setState(this.state);
     }
 
 
     getPageObjects(){
-        var totalPages = this.state.totalItems / this.state.itemsOnPage;
-        if (this.state.totalItems % this.state.itemsOnPage > 0){
+        var totalPages = (this.state.totalItems - this.state.totalItems % this.state.itemsOnPage) / this.state.itemsOnPage;
+        if (this.state.totalItems / this.state.itemsOnPage - totalPages > 0){
             totalPages++;
         }
 
-        var startFromPage = Math.max(0, this.state.currentPage - this.state.itemsOnPage/2)
-        var endPage = Math.min(totalPages, this.state.currentPage + this.state.itemsOnPage/2 + 1)
+        var startFromPage = Math.max(0, this.state.currentPage - this.state.itemsOnPage / 2)
+        var endPage = Math.min(totalPages - 1, this.state.currentPage + this.state.itemsOnPage / 2 + 1)
 
         var result = [];
-        result.push({title: '<', index: Math.max(this.state.currentPage - 1, 0), enabled: this.state.current != 0});
+        result.push({title: '<', index: Math.max(this.state.currentPage - 1, 0), enabled: this.state.currentPage != 0});
 
         var startPageTitle = startFromPage + 1;
         if (startFromPage != 0){
@@ -58,7 +59,7 @@ class Pager extends Component {
             result.push({title: endPageTitle, index: endPage, enabled: endPage != this.state.currentPage});
         }
 
-        result.push({title: '>', index: Math.min(this.state.currentPage + 1, totalPages - 1), enabled: this.state.current != totalPages - 1});
+        result.push({title: '>', index: Math.min(this.state.currentPage + 1, totalPages - 1), enabled: this.state.currentPage != totalPages - 1});
 
         return result;
 
