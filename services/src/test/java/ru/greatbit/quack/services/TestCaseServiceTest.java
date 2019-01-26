46519990957a60c0b0544be22f05e860c9fd5424
azee <inge4hep>
2019-01-27 01:19:20 +1100

    Add single testcase fetch restrictions test

--- services/src/test/java/ru/greatbit/quack/services/TestCaseServiceTest.java
+++ services/src/test/java/ru/greatbit/quack/services/TestCaseServiceTest.java
@@ -3,13 +3,11 @@ package ru.greatbit.quack.services;
 
 import org.junit.Test;
 import ru.greatbit.quack.beans.Filter;
-import ru.greatbit.quack.beans.Project;
+import ru.greatbit.quack.services.errors.EntityAccessDeniedException;
 
-import java.util.List;
-
-import static java.util.stream.Collectors.toList;
-import static org.hamcrest.Matchers.containsInAnyOrder;
+import static junit.framework.TestCase.assertNull;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 
@@ -23,10 +21,28 @@ public class TestCaseServiceTest extends BaseTest{
     }
 
     @Test
+    public void adminCanAccessAllTestCasesTest(){
+        assertNotNull(testCaseService.findOne(adminSession, project1.getId(), testCasesProject1.get(0).getId()));
+        assertNotNull(testCaseService.findOne(adminSession, project2.getId(), testCasesProject2.get(0).getId()));
+        assertNotNull(testCaseService.findOne(adminSession, project3.getId(), testCasesProject3.get(0).getId()));
+    }
+
+    @Test
     public void userCanSeeOnlyHisProjectsTestCasesTest(){
         assertThat(testCaseService.findFiltered(userSession, project1.getId(), new Filter()).size(), is(3));
         assertThat(testCaseService.findFiltered(userSession, project2.getId(), new Filter()).size(), is(3));
         assertThat(testCaseService.findFiltered(userSession, project3.getId(), new Filter()).size(), is(0));
+    }
+
+    @Test
+    public void userCanAccessHisProjectsTestCasesTest(){
+        assertNotNull(testCaseService.findOne(userSession, project1.getId(), testCasesProject1.get(0).getId()));
+        assertNotNull(testCaseService.findOne(userSession, project2.getId(), testCasesProject2.get(0).getId()));
+    }
+
+    @Test(expected = EntityAccessDeniedException.class)
+    public void userCanNotAccessRestrictedProjectsTestCasesTest(){
+        assertNull(testCaseService.findOne(userSession, project3.getId(), testCasesProject3.get(0).getId()));
     }
 
 
