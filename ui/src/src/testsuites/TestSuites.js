99185d3bb68fc27f0dbf5a7e49d4d475533ba113
azee <inge4hep>
2019-01-29 22:40:31 +1100

    Test suites list

--- ui/src/src/testsuites/TestSuites.js
+++ ui/src/src/testsuites/TestSuites.js
@@ -1,17 +1,116 @@
 import React, { Component } from 'react';
 import SubComponent from '../common/SubComponent'
+import queryString from 'query-string';
+import { Link } from 'react-router-dom';
+import axios from "axios";
+import Pager from '../pager/Pager';
 
 class TestSuites extends SubComponent {
+
+    state = {
+        testSuites: [],
+        filter: {
+            skip: 0,
+            limit: 20
+        },
+        pager: {
+            total: 0,
+            current: 0,
+            maxVisiblePage: 7,
+            itemsOnPage: 20
+        }
+    };
+
+
+    constructor(props) {
+        super(props);
+        this.queryToFilter = this.queryToFilter.bind(this);
+        this.filterToQuery = this.filterToQuery.bind(this);
+        this.getTestSuites = this.getTestSuites.bind(this);
+        this.getPager = this.getPager.bind(this);
+        this.handlePageChanged = this.handlePageChanged.bind(this);
+        this.updateUrl = this.updateUrl.bind(this);
+    }
+
+    componentDidMount() {
+        super.componentDidMount();
+        this.queryToFilter();
+        this.getTestSuites();
+        this.getPager();
+    }
+
+    handlePageChanged(newPage) {
+        this.state.pager.current = newPage;
+        this.state.filter.skip = newPage * this.state.pager.itemsOnPage;
+        this.getTestSuites();
+        this.setState(this.state);
+        this.updateUrl();
+    }
+
+    updateUrl(){
+        this.props.history.push("/" + this.props.match.params.project + '/testsuites?' + this.filterToQuery(this.state.filter));
+    }
+
+    getTestSuites(){
+        axios
+            .get("/api/" + this.props.match.params.project + "/testsuite?" + this.filterToQuery(this.state.filter))
+            .then(response => {
+                 this.state.testSuites = response.data;
+                 this.setState(this.state);
+        })
+            .catch(error => console.log(error));
+    }
+
+    getPager(){
+        var countFilter = Object.assign({skip:0, limit:0}, this.state.filter);
+        axios
+            .get("/api/" + this.props.match.params.project + "/testsuite/count?" + this.filterToQuery(countFilter))
+            .then(response => {
+                 this.state.pager.total = response.data;
+                 this.state.pager.total.current = this.state.filter.skip / this.state.filter.limit;
+                 this.state.pager.visiblePage = Math.min(response.data / this.state.pager.itemsOnPage + 1, this.state.pager.maxVisiblePage);
+                 this.setState(this.state);
+        })
+            .catch(error => console.log(error));
+    }
+
+    queryToFilter(){
+        var params = queryString.parse(this.props.location.search);
+        this.state.filter.skip = params.skip || 0;
+        this.state.filter.limit = params.limit || 20;
+        this.setState(this.state);
+    }
+
+    filterToQuery(filter){
+        return Object.keys(filter).
+                    map((key) => {return key + "=" + filter[key]}).join("&");
+    }
+
     render() {
         return (
           <div>
-            Suites
+            <div>
+                {this.state.testSuites.map(function(testSuite){
+                    return (
+                           <div>
+                               <Link to={'/' + this.props.match.params.project + '/testcases?testSuite=' + testSuite.id}>
+                                   {testSuite.name}
+                               </Link>
+                           </div>
+                           );
+                }.bind(this))}
+            </div>
+            <div>
+                  <Pager
+                      totalItems={this.state.pager.total}
+                      currentPage={this.state.pager.current}
+                      visiblePages={this.state.pager.maxVisiblePage}
+                      itemsOnPage={this.state.pager.itemsOnPage}
+                      onPageChanged={this.handlePageChanged}
+                  />
+            </div>
           </div>
         );
-      }
-
-      componentDidMount() {
-        super.componentDidMount();
       }
 
 }
