9f1be56abd9b51ab639fd7bfbbb75f9a681bd63a
azee <inge4hep>
2019-02-23 23:56:43 +1100

    Mongo storage

--- services/src/main/java/ru/greatbit/quack/storage/MongoStorage.java
+++ services/src/main/java/ru/greatbit/quack/storage/MongoStorage.java
@@ -1,23 +1,62 @@
 package ru.greatbit.quack.storage;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import ru.greatbit.quack.beans.Attachment;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MongoStorage implements Storage {
+
+    @Autowired
+    private GridFsTemplate gridOperations;
+
+    @Autowired
+    private MongoDbFactory mongoDbFactory;
+
     @Override
     public Attachment upload(InputStream uploadedInputStream, String fileName, long size) throws IOException {
-        return null;
+        DBObject metaData = new BasicDBObject();
+        metaData.put("name", fileName);
+        try {
+            ObjectId result = gridOperations.store(uploadedInputStream, fileName, "application/text", metaData);
+            return new Attachment().withUrl(result.toString()).withDataSize(size).withTitle(fileName);
+        } finally {
+            if (uploadedInputStream != null) {
+                uploadedInputStream.close();
+            }
+        }
     }
 
     @Override
     public void remove(Attachment attachment) throws IOException {
+        gridOperations.delete(new Query().addCriteria(Criteria.where("_id").is(new ObjectId(attachment.getId()))));
 
     }
 
     @Override
     public InputStream get(Attachment attachment) throws IOException {
-        return null;
+        com.mongodb.client.gridfs.model.GridFSFile file = gridOperations.findOne(
+                new Query().addCriteria(Criteria.where("_id").is(new ObjectId(attachment.getId()))));
+        if (file == null){
+            throw new RuntimeException(String.format("File with id %s not found", attachment.getId()));
+        } else {
+            return new GridFsResource(file, getGridFs().openDownloadStream(file.getObjectId())).getInputStream();
+        }
+    }
+    private GridFSBucket getGridFs() {
+        MongoDatabase db = mongoDbFactory.getDb();
+        return GridFSBuckets.create(db);
     }
 }
