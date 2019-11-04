0246c953eda6d0bc733f80cb18708758f9beb8e2
Aziz Namazov <anamazov@amazon.com>
2019-11-04 16:14:49 +1100

    Use checkoboxes only from the lib

--- ui/src/src/index.js
+++ ui/src/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/components/checkbox.min.css'
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
