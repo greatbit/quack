b36aa92bb0f8ba0cbd9bb14411a8fff68c30df74
Nikolay Karev <karev.n@gmail.com>
2021-07-19 21:53:46 +1000

    Better base API url logic

--- ui/src/src/services/backend.js
+++ ui/src/src/services/backend.js
@@ -2,11 +2,7 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = "include";
 
-export const getApiBaseUrl = (url) =>
-  (process.env.NODE_ENV !== "production"
-    ? "http://localhost:81/api/"
-    : "/api/") + url;
-
+export const getApiBaseUrl = url => (process.env.BASE_API_URL || "/api/") + url
 
 const backend = {
   get: (url, options) => axios.get(getApiBaseUrl(url), options),
