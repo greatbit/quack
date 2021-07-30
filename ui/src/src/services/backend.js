f3bd945e82aea082369ee38f0d71fe16fecac8a5
Nikolay Karev <karev.n@gmail.com>
2021-07-30 23:45:11 +1000

    A fix for DELETE method handling

--- ui/src/src/services/backend.js
+++ ui/src/src/services/backend.js
@@ -5,18 +5,18 @@ const getOptions = options => ({
   credentials: "include",
 });
 
-const fetchInternal = (url, method, options) =>
-  fetch(getApiBaseUrl(url), { ...getOptions(options), method }).then(response => response.json());
+const fetchInternal = (url, method, options) => fetch(getApiBaseUrl(url), { ...getOptions(options), method });
+const fetchJSON = (url, method, options) => fetchInternal(url, method, options).then(response => response.json());
 const getMutationOptions = body => ({
   body: JSON.stringify(body),
   headers: { "content-type": "application/json" },
 });
 
 const backend = {
-  get: (url, options) => fetchInternal(url, "GET", options),
-  post: (url, body) => fetchInternal(url, "POST", getMutationOptions(body)),
+  get: (url, options) => fetchJSON(url, "GET", options),
+  post: (url, body) => fetchJSON(url, "POST", getMutationOptions(body)),
   delete: url => fetchInternal(url, "DELETE"),
-  put: (url, body) => fetchInternal(url, "PUT", getMutationOptions(body)),
+  put: (url, body) => fetchJSON(url, "PUT", getMutationOptions(body)),
 };
 
 export default backend;
