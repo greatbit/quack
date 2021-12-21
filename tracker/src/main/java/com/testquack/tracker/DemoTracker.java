b5d380c279e5fd0955e997e8635e80cc496794f8
Aziz Namazov <anamazov@amazon.com>
2021-12-21 22:13:04 +1100

    Demo and Dummy tracker that do not break integration

--- tracker/src/main/java/com/testquack/tracker/DemoTracker.java
+++ tracker/src/main/java/com/testquack/tracker/DemoTracker.java
@@ -13,27 +13,27 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-public class DummyTracker implements Tracker {
+public class DemoTracker implements Tracker {
 
     @Override
     public Issue getIssue(HttpServletRequest request, Session userSession, String issueId) {
-        return null;
+        throw new NotImplementedException();
     }
 
     @Override
     public Issue createIssue(HttpServletRequest request, Session userSession, Issue issue) {
-        throw new RuntimeException("Tracker integration is not yet implemented");
+        return issue.withId(UUID.randomUUID().toString());
     }
 
     @Override
     public Issue linkIssue(HttpServletRequest request, Session userSession, String issueId) {
-        throw new RuntimeException("Tracker integration is not yet implemented");
+        return new Issue().withId(UUID.randomUUID().toString()).withName("Linked issue");
     }
 
 
     @Override
     public Issue updateIssue(HttpServletRequest request, Session userSession, Issue issue) {
-        return null;
+        throw new NotImplementedException();
     }
 
     @Override
@@ -43,26 +43,33 @@ public class DummyTracker implements Tracker {
 
     @Override
     public List<Issue> suggestIssue(HttpServletRequest request, Session userSession, String issueProject, String text) {
-        return Collections.emptyList();
+        return Arrays.asList(
+                new Issue().withId(UUID.randomUUID().toString()).withName("Issue-1"),
+                new Issue().withId(UUID.randomUUID().toString()).withName("Issue-2"),
+                new Issue().withId(UUID.randomUUID().toString()).withName("Issue-3")
+        );
     }
 
     @Override
     public List<TrackerProject> suggestProjects(HttpServletRequest request, Session userSession, String project, String text) {
-        return Collections.emptyList();
+        return Collections.singletonList(new TrackerProject().withId("Issue").withName("Tracker Project Name"));
     }
 
     @Override
     public List<IssueType> getIssueTypes(HttpServletRequest request, Session userSession, String issueProject) throws Exception {
-        return Collections.emptyList();
+        return Arrays.asList(new IssueType("BUG", "Bug"), new IssueType("TASK", "Task"));
     }
 
     @Override
     public List<IssuePriority> getIssuePriorities(HttpServletRequest request, Session userSession, String issueProject) throws Exception {
-        return Collections.emptyList();
+        return Arrays.asList(new IssuePriority("HIGH", "High"), new IssuePriority("LOW", "Low"));
     }
 
     @Override
     public List<TrackerProject> getAllProjects(HttpServletRequest request, Session userSession, String project) throws Exception {
-        return Collections.emptyList();
+        return Arrays.asList(
+                new TrackerProject().withId("Issue").withName("Tracker Project Name1"),
+                new TrackerProject().withId("Issue2").withName("Tracker Project Name2")
+        );
     }
 }
