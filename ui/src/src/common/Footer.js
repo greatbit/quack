f7f358cb0d0d4ea4bffccbf571e3bfb9b787953a
Aziz Namazov <anamazov@amazon.com>
2022-01-08 00:05:50 +1100

    Prettify footer links

--- ui/src/src/common/Footer.js
+++ ui/src/src/common/Footer.js
@@ -4,11 +4,11 @@ class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <div className="container">
-          <span className="text-muted" className="col-3"><a href="https://greatbit.com.au" target="_blanc">@GreatBit</a></span>
-          <span className="text-muted" className="col-3"><a href="https://www.testquack.com" target="_blanc">TestQuAck.com</a></span>
-          <span className="text-muted" className="col-3"><a href="https://www.testquack.com/#contacts" target="_blanc">Contact Us</a></span>
-          <span className="text-muted" className="col-3"><a href="https://github.com/greatbit/quack/issues/new?assignees=&labels=&template=bug_report.md&title=" target="_blanc">Report a Bug</a></span>
+        <div className="container text-center">
+          <span className="text-muted" className="col-3"><a href="https://greatbit.com.au" className="link-black" target="_blanc">@GreatBit</a></span>
+          <span className="text-muted" className="col-3"><a href="https://www.testquack.com" className="link-black" target="_blanc">TestQuAck.com</a></span>
+          <span className="text-muted" className="col-3"><a href="https://www.testquack.com/#contacts" className="link-black" target="_blanc">Contact Us</a></span>
+          <span className="text-muted" className="col-3"><a href="https://github.com/greatbit/quack/issues/new?assignees=&labels=&template=bug_report.md&title=" className="link-black" target="_blanc">Report a Bug</a></span>
         </div>
       </footer>
     );
